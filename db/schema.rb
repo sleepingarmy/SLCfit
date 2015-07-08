0cac55139f614a25b7982012c4b1677f432eca5a
Megan Grisley <megangrisley@gmail.com>
2015-07-08 16:33:06 -0600

    added most of crud actions for goals_controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150708211733) do
-
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "activities", force: :cascade do |t|
+    t.integer  "week_id"
+    t.string   "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "goals", force: :cascade do |t|
     t.integer  "user_id"
@@ -28,13 +31,6 @@ ActiveRecord::Schema.define(version: 20150708211733) do
     t.boolean  "complete"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-
-  create_table "activities", force: :cascade do |t|
-    t.integer  "week_id"
-    t.string   "type"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-
   end
 
   create_table "users", force: :cascade do |t|
