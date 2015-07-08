5c772b396f6b58d84abcfd09327d0ae64da7a384
Rosemary Thomas <rsm.thomas@gmail.com>
2015-07-08 15:13:43 -0600

    added show action to weeks controller

--- app/controllers/weeks_controller.rb
+++ app/controllers/weeks_controller.rb
@@ -1,8 +1,18 @@
 class WeeksController < ApplicationController
+  before_action :find_user
   before_action :find_activites 
   before_action :find_goal
 
+  def show
+    @week = Week.find_by(id: params[:id])
+  end
+
   private
+
+  def find_user
+    @user = current_user
+  end
+
   def find_activities
     @activities = Activity.find_by(id: params[:id])
   end
