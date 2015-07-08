12bdae1a3b7ff90a3bf32ee7f4cd3763963b9859
Megan Grisley <megangrisley@gmail.com>
2015-07-08 15:16:27 -0600

    added goal model and made migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150708201931) do
+ActiveRecord::Schema.define(version: 20150708210230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "goals", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "description"
+    t.text     "reason"
+    t.integer  "time_frame"
+    t.boolean  "complete"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
