3db197407d4014f85d8a9ee0461092a02d501955
Rosemary Thomas <rsm.thomas@gmail.com>
2015-07-08 15:31:02 -0600

    added edit/update actions to weeks controller.

--- app/controllers/weeks_controller.rb
+++ app/controllers/weeks_controller.rb
@@ -1,10 +1,22 @@
 class WeeksController < ApplicationController
   before_action :find_user
-  # before_action :find_activites 
-  # before_action :find_goal
+  before_action :find_activites 
+  before_action :find_goal
 
   def show
     @week = Week.find_by(id: params[:id])
+  end
+
+  def edit
+    
+  end
+
+  def update
+    if @week.update(week_params)
+      redirect_to (week_path(@week.id))
+    else
+      render :new, status: 400
+    end
   end
 
   private
@@ -13,11 +25,15 @@ class WeeksController < ApplicationController
     @user = current_user
   end
 
-  # def find_activities
-  #   @activities = Activity.find_by(id: params[:id])
-  # end
+  def find_activities
+    @activities = Activity.find_by(id: params[:id])
+  end
 
-  # def find_goal
-  #   @goal = Goal.find_by(id: params[:id])
-  # end
+  def find_goal
+    @goal = Goal.find_by(id: params[:id])
+  end
+
+  def week_params
+    params.require(:week).permit(:activities)
+  end
 end
