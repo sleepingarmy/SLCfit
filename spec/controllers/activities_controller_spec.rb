05e1bd4fd2e63b2e36a2d47ab72e614d2a17e1e7
Rosemary Thomas <rsm.thomas@gmail.com>
2015-07-13 16:24:14 -0600

    activites index spec successful, added activities index view.

--- spec/controllers/activities_controller_spec.rb
+++ spec/controllers/activities_controller_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe ActivitiesController, type: :controller do
 
+
+describe "GET #index" do
+  it "returns http success" do
+    get :index, goal_id: 1, week_id: 1
+    expect(response).to have_http_status(:success)
+  end
+end
+
 end
