3ee713cc418961ba481327c8fedf0ab2a480ff73
Megan Grisley <megangrisley@gmail.com>
2015-07-14 15:44:54 -0600

    added account edit path to navbar
    Change-Id: I9f52247192996699c313de0ad93e38ebf90e550e

---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,22 @@
+# Place all the behaviors and hooks related to the matching controller here.
+# All this logic will automatically be available in application.js.
+# You can use CoffeeScript in this file: http://coffeescript.org/
+
+
+(function(){
+
+  var parallax = document.querySelectorAll(".parallax"),
+      speed = 0.5;
+
+  window.onscroll = function(){
+    [].slice.call(parallax).forEach(function(el,i){
+
+      var windowYOffset = window.pageYOffset,
+          elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
+
+      el.style.backgroundPosition = elBackgrounPos;
+
+    });
+  };
+
+})();
