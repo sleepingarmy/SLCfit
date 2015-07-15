86b6b308b84e2dc856e73a317723f98714b5e2e5
Megan Grisley <megangrisley@gmail.com>
2015-07-15 15:05:32 -0600

    sized images and did some styling
    Change-Id: Iccbf8883f1d8ec8f61d93c11ea7e8f273b778b63

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -3,20 +3,3 @@
 # You can use CoffeeScript in this file: http://coffeescript.org/
 
 
-(function(){
-
-  var parallax = document.querySelectorAll(".parallax"),
-      speed = 0.5;
-
-  window.onscroll = function(){
-    [].slice.call(parallax).forEach(function(el,i){
-
-      var windowYOffset = window.pageYOffset,
-          elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
-
-      el.style.backgroundPosition = elBackgrounPos;
-
-    });
-  };
-
-})();
