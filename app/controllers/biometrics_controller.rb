e1ac88e7969592eca10a87323771287b92396831
Rosemary Thomas <rsm.thomas@gmail.com>
2015-07-21 17:06:28 -0600

    addes strenght and speed arrays to biometrics controller.

--- app/controllers/biometrics_controller.rb
+++ app/controllers/biometrics_controller.rb
@@ -11,7 +11,9 @@ class BiometricsController < ApplicationController
       redirect_to infos_path
     end
 
-    @bio_array = [['date', 'weight'], ]
+     # Biometric.create_arrays(@biometrics, current_user)
+
+     @bio_array = [['date', 'weight'], ]
 
     @biometrics.each do |bio|
       biodate = bio.created_at.strftime "%a %D"
@@ -25,6 +27,22 @@ class BiometricsController < ApplicationController
       biodate = bio.created_at.strftime "%a %D"
       biobmi = bio.weight * 703 / current_user.info.height ** 2
       @bmi_array << [biodate, biobmi]
+    end
+
+    @speed_array = [['date', 'speed'], ]
+
+    @biometrics.each do |bio|
+      biodate = bio.created_at.strftime "%a %D"
+      speed = bio.mile_speed
+      @speed_array << [biodate, speed]
+    end
+
+    @strength_array = [['date', 'strength'], ]
+
+    @biometrics.each do |bio|
+      biodate = bio.created_at.strftime "%a %D"
+      speed = bio.mile_speed
+      @speed_array << [biodate, speed]
     end
   end
 
--- app/models/biometric.rb
+++ app/models/biometric.rb
@@ -5,4 +5,38 @@ class Biometric < ActiveRecord::Base
     order(:created_at)
   end
 
+  # def self.create_arrays(biometrics, current_user)
+  #   @bio_array = [['date', 'weight'], ]
+
+  #   biometrics.each do |bio|
+  #     biodate = bio.created_at.strftime "%a %D"
+  #     bioweight = bio.weight
+  #     @bio_array << [biodate, bioweight]
+  #   end
+
+  #   @bmi_array = [['date', 'bmi'], ]
+
+  #   biometrics.each do |bio|
+  #     biodate = bio.created_at.strftime "%a %D"
+  #     biobmi = bio.weight * 703 / current_user.info.height ** 2
+  #     @bmi_array << [biodate, biobmi]
+  #   end
+
+    # @speed_array = [['date', 'speed'], ]
+
+    # biometrics.each do |bio|
+    #   biodate = bio.created_at.strftime "%a %D"
+    #   speed = bio.mile_speed
+    #   @speed_array << [biodate, speed]
+    # end
+
+    # @strength_array = [['date', 'strength'], ]
+
+    # biometrics.each do |bio|
+    #   biodate = bio.created_at.strftime "%a %D"
+    #   speed = bio.mile_speed
+    #   @speed_array << [biodate, speed]
+    # end
+  # end
+
 end
