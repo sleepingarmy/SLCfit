a6866a5e3595a532ba132c080a6cf122e5d6ce1f
Rosemary Thomas <rsm.thomas@gmail.com>
2015-07-28 14:37:30 -0600

    yelp working, sidebar styling tweaks.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   layout 'splash'
+
   def index
     if user_signed_in?
       redirect_to goals_path
@@ -7,12 +8,13 @@ class WelcomeController < ApplicationController
   end
 
   def search
-       @client = Yelp::Client.new({ consumer_key: ENV['consumer_key'],
-                            consumer_secret: ENV['consumer_secret'],
-                            token: ENV['token'],
-                            token_secret: ENV['token_secret']
-                          })
-    parameters = { term: params[:term], limit: 16 }
+   @client = Yelp::Client.new({ consumer_key: ENV['consumer_key'],
+                        consumer_secret: ENV['consumer_secret'],
+                        token: ENV['token'],
+                        token_secret: ENV['token_secret']
+                      })
+
+    parameters = { term: params[:term], limit: 10 }
     @search_results = JSON.parse(@client.search('Salt Lake City', parameters).to_json)
   end
 end
