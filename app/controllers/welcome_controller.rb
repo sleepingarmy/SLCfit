910cf87ffe484f8d000d270373bb24f565df75c5
hectorpina123@gmail.com <hectorpina123@gmail.com>
2015-07-28 17:48:05 -0600

    moved yelp to application controller
    Change-Id: I4bd8214cae0ca0e4c024eed643f35e1cb37e434d

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
-  # layout 'splash'
+  layout 'splash'
 
   def index
     if user_signed_in?
@@ -7,14 +7,4 @@ class WelcomeController < ApplicationController
     end
   end
 
-  def search
-   @client = Yelp::Client.new({ consumer_key: ENV['CONSUMER_KEY'],
-                        consumer_secret: ENV['CONSUMER_KEY'],
-                        token: ENV['TOKEN'],
-                        token_secret: ENV['TOKEN_SECRET']
-                      })
-
-    parameters = { term: params[:term], limit: 12 }
-    @search_results = JSON.parse(@client.search('Salt Lake City', parameters).to_json)
-  end
 end
