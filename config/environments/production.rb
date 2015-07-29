f9db4c548a1fe43f39e41456adec0306e8a53429
hectorpina123@gmail.com <hectorpina123@gmail.com>
2015-07-28 20:42:33 -0600

    forum sends
    Change-Id: I0db74d842799ee08a4f06012e8b3ae0639123569

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
      :path => '/:class/:attahment/:id_partition/:style/:filename'
    }
 
-  # config.action_mailer.default_url_options = { :host => "www.slcfit.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "http://www.slcfit.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
