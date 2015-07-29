f67a0e89e992841272b2c4eceb474eaaac05efb5
hectorpina123@gmail.com <hectorpina123@gmail.com>
2015-07-28 18:30:07 -0600

    lower case stuff
    Change-Id: Ib169452a23591a82fdeda99b9863f0b76abaaa72

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
-  layout 'splash'
+  # layout 'splash'
 
   def index
     if user_signed_in?
@@ -7,4 +7,22 @@ class WelcomeController < ApplicationController
     end
   end
 
+
+  def search
+    parameters = { term: params[:term], limit: 12 }
+    @search = client.search(params[:location], parameters)
+    # @search_results = JSON.parse(client.search(params[:location], parameters).to_json)
+    binding.pry
+  end
+
+private
+
+  def client
+    @client = Yelp::Client.new({ consumer_key: ENV['CONSUMER_KEY'],
+                        consumer_secret: ENV['CONSUMER_KEY'],
+                        token: ENV['TOKEN'],
+                        token_secret: ENV['TOKEN_SECRET']
+                      })
+    
+  end
 end
