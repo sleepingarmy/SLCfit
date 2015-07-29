93c26c770a7d7e1d8bbc987117afe555274f2238
Rosemary Thomas <rsm.thomas@gmail.com>
2015-07-28 18:25:42 -0600

    changed vars back to lowercase.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,10 +8,10 @@ class WelcomeController < ApplicationController
   end
 
   def search
-   @client = Yelp::Client.new({ consumer_key: ENV['CONSUMER_KEY'],
-                        consumer_secret: ENV['CONSUMER_KEY'],
-                        token: ENV['TOKEN'],
-                        token_secret: ENV['TOKEN_SECRET']
+   @client = Yelp::Client.new({ consumer_key: ENV['consumer_key'],
+                        consumer_secret: ENV['consumer_secret'],
+                        token: ENV['token'],
+                        token_secret: ENV['token_secret']
                       })
 
     parameters = { term: params[:term], limit: 12 }
